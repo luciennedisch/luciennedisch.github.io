806ab26e7b4390f7c2f6a3db98b531150590481d
luciennedisch <dischlucienne@gmail.com>
2026-02-12 18:29:27 -0500

    Update About section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ export const About = () => (
             Lucienne N.Y. Disch
           </h1>
           <p className="text-lg text-gray-700 mt-4 md:mt-12">
-            I am an Economist and currently work as a Postdoctoral Researcher at the Stone Center, University of Chicago
+            I am an Economist with Research Interests in Labor Economics. I currently work as a Postdoctoral Researcher at the Stone Center, University of Chicago
           </p>
           <p className="mt-4 md:mt-12 text-gray-600 max-w-prose mx-auto md:mx-0">
             I am working on how family, school, and neighborhood factors shape
